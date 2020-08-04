77f88f7ba97fe03c2bfbd8bbde235e10cde50ffb
Kashinath Patekar <kashinathpatekar26@gmail.com>
2020-08-04 10:56:51 +0530

    Fix typo and mark all fields required in Transaction

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,13 +1,15 @@
-class Tranaction {
+import 'package:flutter/foundation.dart';
+
+class Transaction {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
 
-  Tranaction({
-    this.id,
-    this.title,
-    this.amount,
-    this.date,
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
   });
 }
