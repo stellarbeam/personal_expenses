466da2bf63b48f89ab78cf8a858099781be83d02
Kashinath Patekar <kashinathpatekar26@gmail.com>
2020-08-04 10:50:00 +0530

    Add transaction model

---
+++ lib/transaction.dart
@@ -0,0 +1,13 @@
+class Tranaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Tranaction({
+    this.id,
+    this.title,
+    this.amount,
+    this.date,
+  });
+}
